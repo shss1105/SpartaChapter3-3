564e94cb9a6a75324b8e27da1513fded3c7d51be
shss1105 <kimseung2331@naver.com>
2024-02-21 14:46:34 +0900

    Feat - 인트로씬 시작메뉴 UI 배치

--- Assets/Scripts/IntroPopup.cs
+++ Assets/Scripts/IntroPopup.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class IntroPopup : UIBase
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
---
+++ Assets/Scripts/StartPopup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPopup : UIBase
+{
+    
+    public void StartGame()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void GameSetting()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+
+    }
+}
