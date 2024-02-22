db6ccb0cd8c53436f092a80188285180240415f0
shss1105 <kimseung2331@naver.com>
2024-02-22 10:26:52 +0900

    Feat - 싱글톤 스크립트 추가

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    private static T _instance;
+//public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+//{
+//    private static T _instance;
 
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<T>();
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    _instance = obj.AddComponent<T>();
-                }
-            }
-            return _instance;
-        }
-    }
-}
+//    public static T Instance
+//    {
+//        get
+//        {
+//            if (_instance == null)
+//            {
+//                _instance = FindObjectOfType<T>();
+//                if (_instance == null)
+//                {
+//                    GameObject obj = new GameObject();
+//                    _instance = obj.AddComponent<T>();
+//                }
+//            }
+//            return _instance;
+//        }
+//    }
+//}
 
 public class UIManager : Singleton<UIManager>
 {
